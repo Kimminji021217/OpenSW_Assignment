8263692085c4b99fbebc6ba97658ab50f1b87980
Minji <rlaminwl02@inu.ac.kr>
2025-11-12 23:17:18 +0900

    add my basic ops

--- week09_git/src/myapp.c
+++ week09_git/src/myapp.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include "myops.h"
 
 double calculate(char *op, double a, double b) {
     printf("Calculating.....\n");
+
+    if (strcmp(op, "+") == 0) {
+        return myadd(a, b);
+    }
+    else if (strcmp(op, "-") == 0) {
+        return mysub(a, b);
+    }
+    else if (strcmp(op, "*") == 0) {
+        return mymul(a, b);
+    }
+    else if (strcmp(op, "/") == 0) {
+        return mydiv(a, b);
+    }
+    
+    else {
+        printf("Unsupported operation: %s\n", op);
+        return 0;
+    }
 }
 
-int main(void) {
-
+int main() {
     double a, b;
     char op[3];
 
@@ -13,6 +32,6 @@ int main(void) {
     printf("Enter expression (e.g. 1 + 2): ");
     scanf("%lf %s %lf", &a, op, &b);
     printf("Result = %.2lf\n", calculate(op, a, b));
-
+    
     return 0;
 }
